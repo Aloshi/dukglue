9ad8c07d231aef24f875d84c62c32457c675c499
marcizhu <marc.izquierdo.hu@gmail.com>
2019-05-29 21:52:47 +0200

    Added time_t as a primitive type

--- include/dukglue/detail_primitive_types.h
+++ include/dukglue/detail_primitive_types.h
@@ -45,6 +45,8 @@ namespace dukglue {
 		DUKGLUE_SIMPLE_VALUE_TYPE(int32_t, duk_is_number, duk_get_int, duk_push_int, value)
 		DUKGLUE_SIMPLE_VALUE_TYPE(int64_t, duk_is_number, duk_get_number, duk_push_number, value) // have to cast to double
 
+		DUKGLUE_SIMPLE_VALUE_TYPE(time_t, duk_is_number, duk_get_number, duk_push_number, value)
+
 		// signed char and unsigned char are surprisingly *both* different from char, at least in MSVC
 		DUKGLUE_SIMPLE_VALUE_TYPE(char, duk_is_number, duk_get_int, duk_push_int, value)
 
